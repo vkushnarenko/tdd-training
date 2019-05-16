f5c1a5a368c2f3d290d4c70d87099ebde762f667
vkushnarenko <vkushnarenko@lohika.com>
2019-05-17 00:16:46 +0300

    task 1, part 2

--- src/string-calc.test.js
+++ src/string-calc.test.js
@@ -12,21 +12,22 @@ const genTest = (title, actual, expected) => {
     })
 }
 
-// 1. int Add(string numbers)
-// ———————————————
-// The method can take up to two numbers, separated by commas, and will return their sum. 
-// for example “” or “1” or “1,2” as inputs.
-// (for an empty string it will return 0) 
-// Hints:
-// ——————
-//  - Start with the simplest test case of an empty string and move to one and two numbers
-//  - Remember to solve things as simply as possible so that you force yourself to write tests you did not think about
-//  - Remember to refactor after each passing test
-// ———————————————————————————————
-
 describe('ADD', () => {
+
+    // 1. int Add(string numbers)
+    // ———————————————
+    // The method can take up to two numbers, separated by commas, and will return their sum. 
+    // for example “” or “1” or “1,2” as inputs.
+    // (for an empty string it will return 0) 
+    // Hints:
+    // ——————
+    //  - Start with the simplest test case of an empty string and move to one and two numbers
+    //  - Remember to solve things as simply as possible so that you force yourself to write tests you did not think about
+    //  - Remember to refactor after each passing test
+    // ———————————————————————————————
+
     genTest(
-        'Should be able to handle empty string and return 0',
+        'Should be able to handle empty string an answer should be 0',
         stringCalc.add(""),
         0
     )
@@ -38,10 +39,23 @@ describe('ADD', () => {
     )
 
     genTest(
-        'Should be able to handle two params and add them',
+        'Should be able to handle two params',
         stringCalc.add('1,2'),
         3
     )
+   
+    // 2. Allow the Add method to handle an unknown amount of numbers
 
+    genTest(
+        'Should be able to handle unknown amount of params',
+        stringCalc.add('1,2,3,5,6,7'),
+        24
+    )
+
+    genTest(
+        'Should be able to handle unknown amount of params',
+        stringCalc.add('1,1,1,1,1,1,1,1,1'),
+        9
+    )
 
   })
